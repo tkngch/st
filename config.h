6cfec1f4c4a12ef317c803ea1a59dc91f14cef6c
Takao Noguchi <tkngch@runbox.com>
2020-02-18 21:32:34 +0000

    change the colors to solarized dark

--- config.h
+++ config.h
@@ -142,44 +142,68 @@ unsigned int tabspaces = 4;
 //   [257] = "#d3d0c8", /* foreground */
 // };
 
+// /* Terminal colors (16 first used in escape sequence) */
+// static const char *colorname[] = {
+//   // embers.light from https://terminal.sexy/
+//
+//   /* 8 normal colors */
+//   [0] = "#16130f", /* black   */
+//   [1] = "#826d57", /* red     */
+//   [2] = "#57826d", /* green   */
+//   [3] = "#6d8257", /* yellow  */
+//   [4] = "#6d5782", /* blue    */
+//   [5] = "#82576d", /* magenta */
+//   [6] = "#576d82", /* cyan    */
+//   [7] = "#a39a90", /* white   */
+//
+//   /* 8 bright colors */
+//   [8]  = "#5a5047", /* black   */
+//   [9]  = "#826d57", /* red     */
+//   [10] = "#57826d", /* green   */
+//   [11] = "#6d8257", /* yellow  */
+//   [12] = "#6d5782", /* blue    */
+//   [13] = "#82576d", /* magenta */
+//   [14] = "#576d82", /* cyan    */
+//   [15] = "#dbd6d1", /* white   */
+//
+//   /* special colors */
+//   [256] = "#dbd6d1", /* background */
+//   [257] = "#433b32", /* foreground */
+// };
+//
+// /*
+//  * Default colors (colorname index)
+//  * foreground, background, cursor
+//  */
+// unsigned int defaultfg = 257;
+// unsigned int defaultbg = 256;
+// static unsigned int defaultcs = 257;
+// static unsigned int defaultrcs = 0;
 
-/* Terminal colors (16 first used in escape sequence) */
+// From st patch: https://st.suckless.org/patches/solarized/
 static const char *colorname[] = {
-  // embers.light from https://terminal.sexy/
-
-  /* 8 normal colors */
-  [0] = "#16130f", /* black   */
-  [1] = "#826d57", /* red     */
-  [2] = "#57826d", /* green   */
-  [3] = "#6d8257", /* yellow  */
-  [4] = "#6d5782", /* blue    */
-  [5] = "#82576d", /* magenta */
-  [6] = "#576d82", /* cyan    */
-  [7] = "#a39a90", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#5a5047", /* black   */
-  [9]  = "#826d57", /* red     */
-  [10] = "#57826d", /* green   */
-  [11] = "#6d8257", /* yellow  */
-  [12] = "#6d5782", /* blue    */
-  [13] = "#82576d", /* magenta */
-  [14] = "#576d82", /* cyan    */
-  [15] = "#dbd6d1", /* white   */
-
-  /* special colors */
-  [256] = "#dbd6d1", /* background */
-  [257] = "#433b32", /* foreground */
+    /* solarized dark */
+    "#073642",  /*  0: black    */
+    "#dc322f",  /*  1: red      */
+    "#859900",  /*  2: green    */
+    "#b58900",  /*  3: yellow   */
+    "#268bd2",  /*  4: blue     */
+    "#d33682",  /*  5: magenta  */
+    "#2aa198",  /*  6: cyan     */
+    "#eee8d5",  /*  7: white    */
+    "#002b36",  /*  8: brblack  */
+    "#cb4b16",  /*  9: brred    */
+    "#586e75",  /* 10: brgreen  */
+    "#657b83",  /* 11: bryellow */
+    "#839496",  /* 12: brblue   */
+    "#6c71c4",  /* 13: brmagenta*/
+    "#93a1a1",  /* 14: brcyan   */
+    "#fdf6e3",  /* 15: brwhite  */
 };
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 0;
+unsigned int defaultfg = 12;
+unsigned int defaultbg = 8;
+static unsigned int defaultcs = 14;
+static unsigned int defaultrcs = 15;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
