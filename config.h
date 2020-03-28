b813ca4180bcc8ca0a4f52b072a80ace352fc349
Takao Noguchi <tkngch@runbox.com>
2020-03-28 08:45:26 +0000

    change fonts

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Inconsolata:pixelsize=15:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "SourceCodePro:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
